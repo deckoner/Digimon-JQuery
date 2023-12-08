881eed9c216de19519af892ba6be75b3f6ac4e15
Deckoner <58949309+deckoner@users.noreply.github.com>
2023-12-08 12:11:17 +0100

    loading 2.0

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,34 @@
-$(window).on('load', function() {
-    const digimonListEndpoint = 'https://digi-api.com/api/v1/digimon';
-    const digimonList = $('#digimonList');
-    const loaderLista = $('.loader');
+const digimonListEndpoint = "https://digi-api.com/api/v1/digimon";
+const digimonList = $("#digimonList");
 
-    function fetchPage(page) {
-        const url = `${digimonListEndpoint}?page=${page}`;
+function fetchPage(page) {
+    const loaderLista = $(".loader");
+    const url = `${digimonListEndpoint}?page=${page}`;
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                if (Array.isArray(data.content)) {
-                    data.content.forEach(digimon => {
-                        const listItem = `<li>${digimon.name} - ID: ${digimon.id}</li>`;
-                        digimonList.append(listItem);
-                    });
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            if (Array.isArray(data.content)) {
+                data.content.forEach((digimon) => {
+                    const listItem = `<li>${digimon.name} - ID: ${digimon.id}</li>`;
+                    digimonList.append(listItem);
+                });
 
-                    // Verificar si hay más páginas y hacer la llamada recursiva
-                    if (page < data.pageable.totalPages - 1) {
-                        fetchPage(page + 1);
-                    } else {
-                        loaderLista.removeClass('loader');
-                        digimonList.show();
-                    }
+                // Verificar si hay más páginas y hacer la llamada recursiva
+                if (page < data.pageable.totalPages - 1) {
+                    fetchPage(page + 1);
                 } else {
-                    console.error('Data format is not as expected:', data);
+                    loaderLista.removeClass("loader");
+                    digimonList.show();
                 }
-            })
-            .catch(error => {
-                console.error('Error fetching Digimon list:', error);
-            });
-    }
+            } else {
+                console.error("Data format is not as expected:", data);
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching Digimon list:", error);
+        });
+}
 
-    // Obtener la información de la primera página (página 0)
-    fetchPage(0);
-});
+// Obtener la información de la primera página (página 0)
+fetchPage(0);
